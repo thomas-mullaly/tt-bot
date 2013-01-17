99d541a657e15f564fcabf4372c38c9ac5a41cdb
Thomas Mullaly <tmullaly88@gmail.com>
2013-01-16 23:21:27 -0500

    Added basic dance command handler

---
+++ bot/commands/Dance.js
@@ -0,0 +1,20 @@
+(function () {
+    "use strict";
+
+    var DanceCommandHandler = function (ttApi) {
+        this.ttApi = ttApi;
+    };
+
+    DanceCommandHandler.prototype.bop = function () {
+        this.ttApi.bop();
+    };
+
+    exports.attachCommandHandler = function (commandsModule, ttApi, utils) {
+        var danceCommandHandler = new DanceCommandHandler(ttApi);
+
+        commandsModule.registerCommandHandler({ botSpecific: true, command: "bop" },
+            utils.proxy(danceCommandHandler, danceCommandHandler.bop));
+        commandsModule.registerCommandHandler({ botSpecific: true, command: "dance" },
+            utils.proxy(danceCommandHandler, danceCommandHandler.parrot));
+    }
+})();
